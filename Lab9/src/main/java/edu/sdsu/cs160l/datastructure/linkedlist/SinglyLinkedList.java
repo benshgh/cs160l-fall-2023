e850de163195772953ab2114a7d94f87d213a8a7
jupdec <mkmeetakapoor@gmail.com>
2023-10-20 13:44:07 -0700

    Adding examples for linkedlist

---
+++ Lab9/src/main/java/edu/sdsu/cs160l/datastructure/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,111 @@
+package edu.sdsu.cs160l.datastructure.linkedlist;
+
+public class SinglyLinkedList {
+
+    Node head;
+    static class Node {
+
+        String data;
+        Node next;
+
+        // Constructor
+        Node(String d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    // Inserting a new node
+    public static SinglyLinkedList insert(SinglyLinkedList list,
+                                    String data)
+    {
+        // Create a new node with given data
+        Node new_node = new Node(data);
+        new_node.next = null;
+
+        if (list.head == null) {
+            list.head = new_node;
+        }
+        else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = new_node;
+        }
+        return list;
+    }
+
+    // Method to print the LinkedList.
+    public static void printList(SinglyLinkedList list)
+    {
+        Node currNode = list.head;
+        System.out.print("SinglyLinkedList: ");
+        // Traversing through the LinkedList
+        while (currNode != null) {
+            System.out.print(currNode.data + " ");
+            currNode = currNode.next;
+        }
+
+        System.out.println();
+    }
+
+    //deleting a node
+    public static SinglyLinkedList deleteByKey(SinglyLinkedList list, String key)
+    {
+        Node currNode = list.head, prev = null;
+
+        if (currNode != null && currNode.data.equals(key)) {
+            list.head = currNode.next; // Changed head
+
+            // Display the message
+            System.out.println(key + " found and deleted");
+
+            // Return the updated List
+            return list;
+        }
+
+        while (currNode != null && !(currNode.data).equals(key)) {
+            prev = currNode;
+            currNode = currNode.next;
+        }
+
+        if (currNode != null) {
+            prev.next = currNode.next;
+            System.out.println(key + " found and deleted");
+        }
+
+        // If key was not present in linked list
+        // currNode should be null
+        if (currNode == null) {
+            System.out.println(key + " not found");
+        }
+
+        // return the List
+        return list;
+    }
+
+
+    public static void main(String[] args)
+    {
+        //Creating a singly linkedlist
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        // Insert the values
+        list = insert(list, "Aditi");
+        list = insert(list, "John");
+        list = insert(list, "Abdul");
+        list = insert(list, "Meeta");
+
+        // Print the LinkedList
+        printList(list);
+
+        //Deleting based on key value
+        deleteByKey(list, "Meeta");
+
+        // Print the LinkedList
+        printList(list);
+
+    }
+}
